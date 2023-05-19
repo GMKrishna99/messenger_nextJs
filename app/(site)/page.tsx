c05faa40d3d7ebc9655c515008c893a9a026f37e
GMKrishna99 <gmohankrishna23@gmail.com>
2023-05-19 13:38:49 +0530

    feat : created Login forms

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import Logo from '../../public/images/logo.png'
+import AuthForm from './components/AuthForm/AuthForm'
 
 export default function Home() {
     return (
-        <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-slate-100'>
+        <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-slate-200'>
             <div className='sm:mx-auto sm:w-full sm:max-w-md'>
                 <Image
                     alt='Logo'
@@ -15,6 +16,7 @@ export default function Home() {
                 <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-slate-800'>Login to your account</h2>
             </div>
             {/* Authentication Form */}
+            <AuthForm />
 
         </div>
     )
