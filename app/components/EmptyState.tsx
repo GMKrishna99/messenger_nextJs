9a2890fc3be598b8f5fe0acf3d76df4200cfc413
GMKrishna99 <gmohankrishna23@gmail.com>
2023-05-25 13:09:49 +0530

    feat : chat conversation state

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,11 @@
+const EmptyState = () => {
+    return (
+        <div className="px-4 py-10 sm:px-6 lg:px-8 h-full flex justify-center items-center bg-slate-100">
+            <div className="text-center items-center flex flex-col">
+                <h3 className="text-slate-400 mt-2 text-xl font-semibold">Select the chat & start Conversation</h3>
+            </div>
+        </div>
+    )
+}
+
+export default EmptyState;
