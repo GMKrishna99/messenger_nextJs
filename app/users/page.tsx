111e00be304eb75793fad575dd60c5cd4d9587ce
GMKrishna99 <gmohankrishna23@gmail.com>
2023-05-25 13:12:58 +0530

    chore : called empty state for user conversation

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+
+import EmptyState from "../components/EmptyState";
+
+const Users = () => {
+    return (
+        <div className="hidden lg:block lg:pl-80 h-full bg-slate-50">
+            <EmptyState />
+        </div>
+
+    )
+}
+
+export default Users;
