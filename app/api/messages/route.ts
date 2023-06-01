cedb812c3774feab0604bb6815d39f098bdd35e7
GMKrishna99 <gmohankrishna23@gmail.com>
2023-06-01 12:56:32 +0530

    feat : pusher added in message route

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,7 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
+import { pusherServer } from "@/app/libs/pusher";
 
 export async function POST(request: Request) {
   try {
@@ -63,9 +64,24 @@ export async function POST(request: Request) {
         },
       },
     });
+
+    // pusher config for realtime messages
+    await pusherServer.trigger(conversationId, "messages:new", newMessage);
+    // extracting the last message
+    const lastMessage =
+      updatedConversation.messages[updatedConversation.messages.length - 1];
+
+    // updated conversation
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, "conversation:update", {
+        id: conversationId,
+        messages: [lastMessage],
+      });
+    });
+
     return NextResponse.json(newMessage);
   } catch (error: any) {
     console.log(error, "ERROR_MESSAGES");
-    return new NextResponse("InternalError", { status: 500 });
+    return new NextResponse("Error", { status: 500 });
   }
 }
