3da837cf2202a0670705e1922f5aadf1fecf529a
GMKrishna99 <gmohankrishna23@gmail.com>
2023-06-01 11:49:55 +0530

    chore : loading modal to layout

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -34,18 +35,23 @@ const UserBox: React.FC<UserBoxProps> = ({
     }, [data, router])
 
     return (
-        <div className="w-full relative flex items-center space-x-3 bg-slate-50 p-3 hover:bg-slate-200 rounded-lg hover:border-transparent transition cursor-pointer " onClick={handleClick}>
-            <Avatar
-                user={data}
-            />
-            <div className="min-w-0 flex-1">
-                <div className="focus:outline-none">
-                    <div className="flex justify-between items-center mb-1">
-                        <p className="text-sm font-bold text-slate-900 capitalize">{data.name}</p>
+        <>
+            {isLoading && (
+                <LoadingModal />
+            )}
+            <div className="w-full relative flex items-center space-x-3 bg-slate-50 p-3 hover:bg-slate-200 rounded-lg hover:border-transparent transition cursor-pointer " onClick={handleClick}>
+                <Avatar
+                    user={data}
+                />
+                <div className="min-w-0 flex-1">
+                    <div className="focus:outline-none">
+                        <div className="flex justify-between items-center mb-1">
+                            <p className="text-sm font-bold text-slate-900 capitalize">{data.name}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
 
     )
 }
